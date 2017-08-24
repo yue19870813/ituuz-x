ad86c7d853f154b3947aa31be4f0b804b2c93ed2
ituuz <yue19870813@163.com>
2017-08-24 16:55:36 +0800

    add Queue

---
+++ ituuzx/util/Queue.ts
@@ -0,0 +1,50 @@
+/// <reference path="./../base/IDestory.ts" />
+namespace itz {
+    /**
+     * Queue
+     * @author ituuz
+     * @since 2017.07.12
+     * Notice:It's not fit for find in big data, because it's use Array implementation. Need algorithm to optimize.
+     */
+    export class Queue<T> {
+        private _elements:Array<T> = null;
+
+		public constructor() {
+			this._elements = new Array<T>();
+        }
+        
+        public push(ele:T):void {
+            this._elements.push(ele);
+        }
+
+        public pop():T {
+            return this._elements.shift();
+        }
+
+        public front():T {
+            if (this._elements.length > 0) {
+                return this._elements[0];
+            }
+            return null;
+        }
+
+        public back() {
+            if (this._elements.length > 0) {
+                return this._elements[this._elements.length - 1];
+            }
+            return null;
+        }
+
+        public isEmpty():boolean {
+            return (this._elements.length < 1);
+        }
+
+        public size():number {
+            return this._elements.length;
+        }
+
+        public clear():void {
+            this._elements.length = 0;
+        }
+    }
+}
