eca87667c519eece8728588464ad160b45a21a88
yue19870813 <yue19870813@163.com>
2018-07-24 13:16:26 +0800

    update py

--- tools/itz.py
+++ tools/itz.py
@@ -1,12 +1,13 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*- 
 import os
 import sys
 import string 
 
-for i in range(1, len(sys.argv)):
-	print "param:", i, sys.argv[i]
+# for i in range(1, len(sys.argv)):
+# 	print "param:", i, sys.argv[i]
 	
 os.system("tsc -p ../ituuzx/tsconfig.json")
 
 # OVER
-os.system('pause')
+# os.system('pause')
